d4920006d52620c57f0ca419528d7d6f8293e448
dinogomez <dinogomez117@gmail.com>
2021-05-01 00:51:15 +0800

    Refactored Solution

--- Are They The Same/run.py
+++ Are They The Same/run.py
@@ -1,37 +1,24 @@
-    def comp2(array1, array2):
-        
-        for x in array2:
-            if x is None:
-                return False
-            y = math.sqrt(x)
-            if((y) in array1):
-                print(str(y) + ", exist in Array 1")
-            else:
-                return False
-        return True
-        
-    
-        
-    # Parsing the first array    
-    def comp1(array1, array2):
-        for x in array1:     
-            if x is None:
-                return False
-            y = x*x
-            if((y) in array2):
-                print(str(y) + ", exist in Array 2")
-            else:
-                return False
-            
-        return True  
-               
-    
-    try:
-        
-        if(comp1(array1, array2) and comp2(array1, array2)):
-            return True
-        else:
-            return False
-    except:
-        
+import math
+def comp(array1, array2): 
+    print("unparsed Array 1:" + str(array1))
+    print("unparsed Array 2:" + str(array2))
+    if array1 is None or array2 is None:
         return False
+    if(len(array1) == 0 and len(array2) == 0):
+        return True
+    elif(len(array1) == 0 or len(array2) == 0):
+        return False
+    counter = 0;
+    for i in array1[:]:
+        x = i*i
+        print(i)
+        print(x)
+        if(x in array2):
+            array2.remove(x)
+            array1.remove(i)
+        print(array1)
+        print(array2)
+    if(len(array1) and len(array2)):
+        return False
+    else:
+        return True
